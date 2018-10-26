78d26ed3c3bd45bfdf7eb859de151dbbf89e40a6
Pyrdacor <trobt@web.de>
2018-10-26 08:56:45 +0200

    Started porting buffer, data and data source

---
+++ Freeserf.net/Buffer.cs
@@ -0,0 +1,261 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+
+namespace Freeserf
+{
+    using Endianess = Endian.Endianess;
+
+    internal static class TypeSize<T>
+    {
+        public readonly static int Size;
+
+        static TypeSize()
+        {
+            var dm = new DynamicMethod("SizeOfType", typeof(int), new Type[] { });
+            ILGenerator il = dm.GetILGenerator();
+            il.Emit(OpCodes.Sizeof, typeof(T));
+            il.Emit(OpCodes.Ret);
+            Size = (int)dm.Invoke(null, null);
+        }
+    }
+
+    unsafe public class Buffer
+    {
+        protected byte* data;
+        protected int size;
+        protected bool owned;
+        protected Buffer parent;
+        protected byte* read;
+        protected Endianess endianess;
+
+        public Buffer(Endianess endianess)
+        {
+
+        }
+
+        public Buffer(byte* data, int size, Endianess endianess)
+        {
+
+        }
+
+        public Buffer(Buffer parent, int start, int length)
+        {
+
+        }
+
+        public Buffer(Buffer parent, int start, int length, Endianess endianess)
+        {
+
+        }
+
+        public Buffer(string path, Endianess endianess)
+        {
+
+        }
+
+        public int Size => size;
+        public byte* Data => data;
+
+        public byte* Unfix()
+        {
+
+        }
+
+        public void SetEndianess(Endianess endianess)
+        {
+            this.endianess = endianess;
+        }
+
+        public bool Readable()
+        {
+
+        }
+
+        public Buffer Pop(int size)
+        {
+
+        }
+
+        public Buffer PopTail()
+        {
+
+        }
+
+        object PopInternal(int size)
+        {
+            object result = null;
+
+            switch (size)
+            {
+                case 1:
+                    result = *read;
+                    break;
+                case 2:
+                    result = *(ushort*)read;
+                    break;
+                case 4:
+                    result = *(uint*)read;
+                    break;
+                case 8:
+                    result = *(ulong*)read;
+                    break;
+                default:
+                    throw new NotSupportedException("Invalid type size.");
+            }
+
+            read += size;
+
+            return result;
+        }
+
+        public T Pop<T>() where T : struct
+        {
+            T value = (T)Convert.ChangeType(PopInternal(TypeSize<T>.Size), typeof(T));
+
+            return (endianess == Endianess.Big) ? Endian.Betoh(value) : Endian.Letoh(value);
+        }
+
+        public Buffer GetSubBuffer(int offset, int size)
+        {
+
+        }
+
+        public Buffer GetTail(int offset)
+        {
+
+        }
+
+        public static explicit operator string(Buffer buffer)
+        {
+            return new string((char*)data, 0, size);
+        }
+
+        public bool Write(string path)
+        {
+
+        }
+
+        protected byte* Offset(int offset)
+        {
+            return (byte*)data + offset;
+        }
+    }
+
+    unsafe public class MutableBuffer : Buffer
+    {
+        protected int reserved;
+        protected int growth;
+
+        public MutableBuffer(Endianess endianess)
+        {
+
+        }
+
+        public MutableBuffer(int size, Endianess endianess)
+        {
+
+        }
+
+        public void Push(Buffer buffer)
+        {
+
+        }
+
+        public void Push(byte* data, int size)
+        {
+
+        }
+
+        public void Push(string str)
+        {
+
+        }
+
+        public void Push(char* str)
+        {
+            Push(new string(str));
+        }
+
+        public void Push<T>(T value, int count = 1)
+        {
+            CheckSize(size + (TypeSize<T>.Size * count));
+
+            for (int i = 0; i < count; i++)
+            {
+                byte* pointer = Offset(size);
+
+                T pointerValue = (T)Convert.ChangeType(pointer, typeof(T));
+                Offset(size)
+
+                *(reinterpret_cast<T*>(offset(size))) = (endianess == EndianessBig) ?
+                                                          htobe(value) :
+                                                          htole(value);
+                size += sizeof(T);
+            }
+        }
+
+        protected void CheckSize(int size)
+        {
+
+        }
+
+        static void ConvertTo<T>(byte* pointer, T value, bool movePointer) where T : struct
+        {
+            object destination = null;
+            int size = TypeSize<T>.Size;
+
+            switch (size)
+            {
+                case 1:
+                    *(byte*)pointer = *(byte*)value;
+                    break;
+                case 2:
+                    destination = *(ushort*)pointer;
+                    break;
+                case 4:
+                    destination = *(uint*)pointer;
+                    break;
+                case 8:
+                    destination = *(ulong*)pointer;
+                    break;
+                default:
+                    throw new NotSupportedException("Invalid type size.");
+            }
+
+            destination = value;
+        }
+
+        static T ConvertFrom<T>(byte* pointer, bool movePointer)
+        {
+            object result = null;
+            int size = TypeSize<T>.Size;
+
+            switch (size)
+            {
+                case 1:
+                    result = *(byte*)pointer;
+                    break;
+                case 2:
+                    result = *(ushort*)pointer;
+                    break;
+                case 4:
+                    result = *(uint*)pointer;
+                    break;
+                case 8:
+                    result = *(ulong*)pointer;
+                    break;
+                default:
+                    throw new NotSupportedException("Invalid type size.");
+            }
+
+            if (movePointer)
+                pointer += size;
+
+            return (T)Convert.ChangeType(result, typeof(T));
+        }
+    }
+}
+*/
---
+++ Freeserf.net/DataSource.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Freeserf
+{
+    // Sprite object.
+    // Contains BGRA data.
+    public class Sprite
+    {
+        public struct Color
+        {
+            byte blue;
+            byte green;
+            byte red;
+            byte alpha;
+        }
+
+        protected int deltaX;
+        protected int deltaY;
+        protected int offsetX;
+        protected int offsetY;
+        protected int width;
+        protected int height;
+        protected byte[] data;
+
+        public Sprite()
+        {
+
+        }
+
+        public Sprite(uint w, uint h)
+        {
+
+        }
+
+        public virtual byte[] GetData()
+        {
+            return data;
+        }
+
+        public virtual int Width => width;
+        public virtual int Height => height;
+        public virtual int DeltaX => deltaX;
+        public virtual int DeltaY => deltaY;
+        public virtual int OffsetX => offsetX;
+        public virtual int OffsetY => offsetY;
+
+        public virtual Sprite GetMasked(Sprite mask)
+        {
+
+        }
+
+        public virtual Sprite CreateMask(Sprite other)
+        {
+
+        }
+
+        public virtual void fill(Sprite::Color color)
+        {
+
+        }
+
+        public virtual void fill_masked(Sprite::Color color)
+        {
+
+        }
+
+        public virtual void add(PSprite other)
+        {
+
+        }
+
+        public virtual void del(PSprite other)
+        {
+
+        }
+
+        public virtual void blend(PSprite other)
+        {
+
+        }
+
+        public virtual void make_alpha_mask()
+        {
+
+        }
+
+        public virtual void Stick(Sprite sticker, uint x, uint y)
+        {
+
+        }
+
+        public static ulong CreateID(ulong resource, ulong index,
+                          ulong maskResource, ulong maskIndex,
+                          Color color)
+        {
+        }
+
+        protected void Create(int width, int height)
+        {
+
+        }
+    }
+
+    public class Animation
+    {
+        public byte Sprite;
+        public int X;
+        public int Y;
+    }
+
+    public abstract class DataSource
+    {
+        public:
+      typedef std::tuple<PSprite, PSprite> MaskImage;
+
+        protected string path;
+        protected bool loaded;
+        protected List<List<Animation>> animationTable = new List<List<Animation>>();
+
+        public DataSource(string path)
+        {
+
+        }
+
+        public abstract string Name { get; }
+        public virtual string Path => path;
+        public virtual bool IsLoaded => loaded;
+        public virtual uint Scale => 0;
+        public virtual uint BPP => 0;
+
+        public abstract bool Check();
+        public abstract bool Load();
+
+        public virtual Sprite GetSprite(Data.Resource resource, int index, Sprite.Color color)
+        {
+
+        }
+
+        public abstract Tuple<Sprite, Sprite> GetSpriteParts(Data.Resource resource, int index);
+
+        public virtual int GetAnimationPhaseCount(int animation)
+        {
+
+        }
+
+        public virtual Animation GetAnimation(int animation, int phase)
+        {
+
+        }
+
+        public abstract Buffer GetSound(int index);
+        public abstract Buffer GetMusic(int index);
+
+        public bool CheckFile(string path)
+        {
+
+        }
+
+        protected Tuple<Sprite, Sprite> separate_sprites(PSprite s1, PSprite s2);
+    }
+}
