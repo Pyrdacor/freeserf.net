102b00101f4071e675afd71206f5f19e68159e35
Pyrdacor <trobt@web.de>
2018-10-28 20:04:00 +0100

    Ported resource.h

---
+++ Freeserf.net/Resource.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Freeserf.Resource
+{
+    public enum Type
+    {
+        None = -1,
+
+        Fish = 0,
+        Pig,
+        Meat,
+        Wheat,
+        Flour,
+        Bread,
+        Lumber,
+        Plank,
+        Boat,
+        Stone,
+        IronOre,
+        Steel,
+        Coal,
+        GoldOre,
+        GoldBar,
+        Shovel,
+        Hammer,
+        Rod,
+        Cleaver,
+        Scythe,
+        Axe,
+        Saw,
+        Pick,
+        Pincer,
+        Sword,
+        Shield,
+
+        GroupFood
+    }
+}
