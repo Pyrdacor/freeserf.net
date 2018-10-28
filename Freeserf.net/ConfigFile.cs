7f914b6b6e731d22bc8007b32d76e5364d08ee3c
Pyrdacor <trobt@web.de>
2018-10-28 20:08:44 +0100

    Added config file dummy class

---
+++ Freeserf.net/ConfigFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Freeserf
+{
+    public class ConfigFile
+    {
+        public bool Save(string path)
+        {
+
+        }
+
+        public bool Write(BinaryWriter writer)
+        {
+
+        }
+
+        public void Read(BinaryReader reader)
+        {
+
+        }
+    }
+}
