68fa3ae00fedce5aea08909bad5021182ebf8971
Pyrdacor <trobt@web.de>
2018-11-02 17:11:09 +0100

    Added random

---
+++ Freeserf.net/Random.cs
@@ -0,0 +1,137 @@
+﻿/*
+ * Random.cs - Random number generator
+ *
+ * Copyright (C) 2012  Jon Lund Steffensen <jonlst@gmail.com>
+ * Copyright (C) 2018  Robert Schneckenhaus <robert.schneckenhaus@web.de>
+ *
+ * This file is part of freeserf.net. freeserf.net is based on freeserf.
+ *
+ * freeserf.net is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * freeserf.net is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with freeserf.net. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Freeserf
+{
+    class Random
+    {
+        System.Random internalRandom;
+        ushort[] state = new ushort[3];
+
+        ushort RandomInt()
+        {
+            return (ushort)internalRandom.Next(0, 65536);
+        }
+
+        public Random()
+        {
+            internalRandom = new System.Random(DateTime.Now.Millisecond);
+
+            state[0] = RandomInt();
+            state[1] = RandomInt();
+            state[2] = RandomInt();
+
+            random();
+        }
+
+        public Random(ushort value)
+        {
+            state[0] = value;
+            state[1] = value;
+            state[2] = value;
+        }
+
+        public Random( Random randomState)
+        {
+            state[0] = randomState.state[0];
+            state[1] = randomState.state[1];
+            state[2] = randomState.state[2];
+        }
+
+        public Random(string str)
+        {
+            var bytes = Encoding.ASCII.GetBytes(str);
+            ulong tmp = 0;
+
+            for (int i = 15; i >= 0; --i)
+            {
+                tmp <<= 3;
+                byte c = (byte)(bytes[i] - (byte)'0' - 1);
+                tmp |= c;
+            }
+
+            state[0] = (ushort)(tmp & 0xFFFF);
+            tmp >>= 16;
+            state[1] = (ushort)(tmp & 0xFFFF);
+            tmp >>= 16;
+            state[2] = (ushort)(tmp & 0xFFFF);
+        }
+
+        public Random(ushort base0, ushort base1, ushort base2)
+        {
+            state[0] = base0;
+            state[1] = base1;
+            state[2] = base2;
+        }
+
+        ushort random()
+        {
+            ushort[] rnd = state;
+            ushort r = (ushort)((rnd[0] + rnd[1]) ^ rnd[2]);
+            rnd[2] += rnd[1];
+            rnd[1] ^= rnd[2];
+            rnd[1] = (ushort)((rnd[1] >> 1) | (rnd[1] << 15));
+            rnd[2] = (ushort)((rnd[2] >> 1) | (rnd[2] << 15));
+            rnd[0] = r;
+
+            return r;
+        }
+
+        public override string ToString()
+        {
+            ulong tmp0 = state[0];
+            ulong tmp1 = state[1];
+            ulong tmp2 = state[2];
+
+            string str = "";
+
+            ulong tmp = tmp0;
+            tmp |= tmp1 << 16;
+            tmp |= tmp2 << 32;
+
+            for (int i = 0; i < 16; ++i)
+            {
+                byte c = (byte)(tmp & 0x07);
+                c += (byte)'1';
+                str += c;
+                tmp >>= 3;
+            }
+
+            return str;
+        }
+
+        public static Random operator ^(Random left, Random right)
+        {
+            ushort s0 = (ushort)(left.state[0] ^ right.state[0]);
+            ushort s1 = (ushort)(left.state[1] ^ right.state[1]);
+            ushort s2 = (ushort)(left.state[2] ^ right.state[2]);
+
+            return new Random(s0, s1, s2);
+        }
+    }
+}
