3f18433656a01db3528a0117d64511fce8ee23a7
Pyrdacor <trobt@web.de>
2018-11-04 07:44:44 +0100

    Added dummies for graphics and frames

---
+++ Freeserf.net/Graphics.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Freeserf
+{
+    public class Graphics
+    {
+        static Graphics instance = null;
+
+        public static Graphics Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Graphics();
+
+                return instance;
+            }
+        }
+
+        Graphics()
+        {
+
+        }
+
+        public IFrame CreateFrame(uint width, uint height)
+        {
+
+        }
+    }
+}
