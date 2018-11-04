3841db36361e59725aac739719a54d065b2f0723
Pyrdacor <trobt@web.de>
2018-11-04 09:40:31 +0100

    Added GameInfo dummy and ported GameManager

--- Freeserf.net/GameManager.cs
+++ Freeserf.net/GameManager.cs
@@ -35,5 +35,98 @@ namespace Freeserf
             void OnNewGame(Game game);
             void OnEndGame(Game game);
         }
+
+        Game currentGame = null;
+        readonly List<IHandler> handlers = new List<IHandler>();
+
+        static GameManager instance = null;
+
+        public static GameManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new GameManager();
+
+                return instance;
+            }
+        }
+
+        GameManager()
+        {
+
+        }
+
+        public void AddHandler(IHandler handler)
+        {
+            handlers.Add(handler);
+        }
+
+        public void DeleteHandler(IHandler handler)
+        {
+            handlers.Remove(handler);
+        }
+
+        public Game GetCurrentGame()
+        {
+            return currentGame;
+        }
+
+        public bool StartRandomGame()
+        {
+            return StartGame(new GameInfo(new Random()));
+        }
+
+        public bool StartGame(GameInfo gameInfo)
+        {
+            Game newGame = gameInfo.Instantiate();
+
+            if (newGame == null)
+            {
+                return false;
+            }
+
+            SetCurrentGame(newGame);
+
+            return true;
+        }
+
+        public bool LoadGame(string path)
+        {
+            Game newGame = new Game();
+
+            if (!GameStore.Instance.Load(path, newGame))
+            {
+                return false;
+            }
+
+            SetCurrentGame(newGame);
+            newGame.Pause();
+
+            return true;
+        }
+
+        void SetCurrentGame(Game newGame)
+        {
+            if (currentGame != null)
+            {
+                foreach (IHandler handler in handlers)
+                {
+                    handler.OnEndGame(currentGame);
+                }
+            }
+
+            currentGame = newGame;
+
+            if (currentGame == null)
+            {
+                return;
+            }
+
+            foreach (IHandler handler in handlers)
+            {
+                handler.OnNewGame(currentGame);
+            }
+        }
     }
 }
