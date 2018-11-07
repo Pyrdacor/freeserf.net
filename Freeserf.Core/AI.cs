c520039fcc670f79d2913a1c0dbfffef6318f0f1
Pyrdacor <trobt@web.de>
2018-11-07 15:02:26 +0100

    Added AI dummy file

---
+++ Freeserf.Core/AI.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Freeserf
+{
+    /// <summary>
+    /// Note: The ai will not be the same as in the original game.
+    /// But it will reflect some of the ai character descriptions.
+    /// The plan is to add some new ai characters with better ai.
+    /// 
+    /// Maybe the campaign will be extended for this sake as well.
+    /// </summary>
+    public class AI
+    {
+        /// <summary>
+        /// How aggressive (2 = very aggressive)
+        /// </summary>
+        int aggressivity = 0; // 0 - 2
+        /// <summary>
+        /// How skilled in fights (2 = skilled fighter)
+        /// </summary>
+        int militarySkill = 0; // 0 - 2
+        /// <summary>
+        /// How much focus on military (2 = high focus)
+        /// </summary>
+        int militaryFocus = 0; // 0 - 2
+        /// <summary>
+        /// How much focus on expanding (2 = aggressive expansion)
+        /// </summary>
+        int expandFocus = 0; // 0 - 2
+        /// <summary>
+        /// How much focus at having much buildings
+        /// </summary>
+        int buildingFocus = 0; // 0 - 2
+        /// <summary>
+        /// How much focus at having much gold
+        /// </summary>
+        int goldFocus = 0; // 0 - 2
+        /// <summary>
+        /// The ai will try to gather prioritized food if possible
+        /// </summary>
+        int[] foodSourcePriorities = new int[3]; // 0 - 2 for fish, bread and meat
+        /// <summary>
+        /// The ai will try to build prioritized military buildings if possible
+        /// </summary>
+        int[] militaryBuildingPriorities = new int[3]; // 0 - 2 for hut, tower and fortress
+        int[] minPlanksForMilitaryBuildings = new int[2]; // minimum free planks for tower and fortress
+        int[] minStonesForMilitaryBuildings = new int[2]; // minimum free stones for tower and fortress
+
+        /* Military Focus
+         * 
+         * - Focuses on getting coal and iron
+         * - Focuses on crafting weapons and shields
+         * - High knight production
+         * - Many military buildings
+         */
+
+        /* Military Skill
+         * 
+         * - Will protect important buildings with some military buildings
+         * - Will have enough knights where needed
+         * - Will try to attack strategic spots of the enemy
+         * - Prefers attacking spots of the enemy that are able to capture
+         */
+
+        /// <summary>
+        /// This is called if an enemy is in range to attack the castle.
+        /// </summary>
+        /// <param name="lastChance">If only the castle and a few buildings remain. Only set for better ai characters.</param>
+        public void PrepareForDefendingCastle(bool lastChance)
+        {
+            // Order all knights to castle
+            // - Set maximum knights of military buildings to minimum
+            // - Order knights back to castle
+
+            // If lastChance use drastic methods like burning down military buildings
+            if (lastChance)
+            {
+                // Burn down all military buildings to free knights
+            }
+        }
+
+        public void PrepareFights(bool quick)
+        {
+            // Set maximum knights of military buildings to maximum
+
+            if (!quick)
+            {
+                // If bad knights are in military buildings and there are better
+                // ones in inventories, than swap with better ones
+            }
+        }
+    }
+}
