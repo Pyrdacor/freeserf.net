c2995bac259a9206398ffa6ad96e4ffb6e9835fa
Pyrdacor <trobt@web.de>
2018-11-07 16:29:54 +0100

    Added dummy render classes for serfs and map tiles

---
+++ Freeserf.Core/Render/RenderMapTile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Freeserf.Render
+{
+    using MapPos = UInt32;
+
+    public class RenderMapTile
+    {
+        MapPos mapPos = 0;
+        Map.Terrain terrain = Map.Terrain.Grass0;
+        ITriangle triangle = null;
+
+        public RenderMapTile(MapPos mapPos, Map.Terrain terrain, ITriangle triangle)
+        {
+            this.mapPos = mapPos;
+            this.terrain = terrain;
+            this.triangle = triangle;
+        }
+
+        public void Delete()
+        {
+            triangle.Delete();
+            triangle = null;
+        }
+
+        public void Draw(Rect visibleMapArea)
+        {
+            if (triangle == null)
+                return;
+
+            // TODO: if tile is outside the visible map area -> return
+            // TODO: set position and texture coords based on:
+            // mapPos
+            // terrain
+        }
+    }
+}
---
+++ Freeserf.Core/Render/RenderSerf.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Freeserf.Render
+{
+    public class RenderSerf
+    {
+        Serf serf = null;
+        ISprite sprite = null;
+
+        public RenderSerf(Serf serf, ISprite sprite)
+        {
+            this.serf = serf;
+            this.sprite = sprite;
+        }
+
+        public void Delete()
+        {
+            sprite.Delete();
+            sprite = null;
+            serf = null;
+        }
+
+        public void Draw(Rect visibleMapArea)
+        {
+            if (sprite == null || serf == null)
+                return;
+
+            // TODO: if serf is outside the visible map area -> return
+            // TODO: set position and texture coords based on:
+            // serf.Position
+            // serf.Animation
+            // serf.Counter
+        }
+    }
+}
