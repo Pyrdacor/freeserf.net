30cbbc448d3bd7748c940b5f2742f0d3fde97331
Pyrdacor <trobt@web.de>
2018-11-07 16:05:44 +0100

    Added license headers, removed unused usings. Renamed StaticRenderBuffer.cs to RenderBuffer.cs.

--- Freeserf.Renderer.OpenTK/RenderLayer.cs
+++ Freeserf.Renderer.OpenTK/RenderLayer.cs
@@ -1,6 +1,26 @@
-﻿using System;
+﻿/*
+ * RenderLayer.cs - Render layer implementation
+ *
+ * Copyright (C) 2018  Robert Schneckenhaus <robert.schneckenhaus@web.de>
+ *
+ * This file is part of freeserf.net. freeserf.net is based on freeserf.
+ *
+ * freeserf.net is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * freeserf.net is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with freeserf.net. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using OpenTK.Graphics.OpenGL;
 using Freeserf.Render;
 
@@ -18,13 +38,13 @@ namespace Freeserf.Renderer.OpenTK
             set;
         } = null;
 
-        readonly StaticRenderBuffer renderBuffer = null;
+        readonly RenderBuffer renderBuffer = null;
         readonly Dictionary<Size, List<IRenderNode>> nodes = new Dictionary<Size, List<IRenderNode>>();
         readonly Texture texture = null;
 
         public RenderLayer(Layer layer, Shape shape, Texture texture, Color colorKey = null)
         {
-            renderBuffer = new StaticRenderBuffer(shape);
+            renderBuffer = new RenderBuffer(shape);
 
             Layer = layer;
             this.texture = texture;
--- Freeserf.Renderer.OpenTK/Sprite.cs
+++ Freeserf.Renderer.OpenTK/Sprite.cs
@@ -1,7 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using OpenTK.Graphics.OpenGL;
+﻿/*
+ * Sprite.cs - Textured sprite
+ *
+ * Copyright (C) 2018  Robert Schneckenhaus <robert.schneckenhaus@web.de>
+ *
+ * This file is part of freeserf.net. freeserf.net is based on freeserf.
+ *
+ * freeserf.net is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * freeserf.net is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with freeserf.net. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 using Freeserf.Render;
 
 namespace Freeserf.Renderer.OpenTK
