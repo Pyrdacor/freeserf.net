ec30109043d811729f062915478c9322f55ad464
Pyrdacor <trobt@web.de>
2018-11-08 15:50:00 +0100

    Added texture atlas interface.

---
+++ Freeserf.Core/Render/ITextureAtlas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Freeserf.Render
+{
+    public interface ITextureAtlas
+    {
+        void AddSprite(uint spriteIndex, int width, int height, byte[] data);
+        Position GetOffset(uint spriteIndex);
+    }
+}
