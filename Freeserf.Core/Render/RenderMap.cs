1cd9f890bd5dc99494ad05c5d194858d9449d305
Pyrdacor <trobt@web.de>
2018-11-19 17:56:17 +0100

    Added missing license comment

--- Freeserf.Core/Render/RenderMap.cs
+++ Freeserf.Core/Render/RenderMap.cs
@@ -1,5 +1,25 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿/*
+ * RenderMap.cs - Handles map rendering
+ *
+ * Copyright (C) 2018  Robert Schneckenhaus <robert.schneckenhaus@web.de>
+ *
+ * This file is part of freeserf.net. freeserf.net is based on freeserf.
+ *
+ * freeserf.net is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * freeserf.net is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with freeserf.net. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.Collections.Generic;
 
 /*
  * The texture atlas for map tile graphics contains 155 sprites.
@@ -357,6 +377,12 @@ namespace Freeserf.Render
             y -= 4 * (int)map.GetHeight(pos);
 
             return new Position(x, y);
+        }
+
+        public MapPos GetMapPosFromMousePosition(Position position)
+        {
+            // TODO
+            return 0;
         }
 
         void UpdatePosition()
