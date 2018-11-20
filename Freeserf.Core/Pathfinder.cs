49701146508433da5cdf8b449d843b725a2e0038
Pyrdacor <trobt@web.de>
2018-11-20 10:20:49 +0100

    Ported pathfinder

--- Freeserf.Core/Pathfinder.cs
+++ Freeserf.Core/Pathfinder.cs
@@ -20,16 +20,279 @@
  * along with freeserf.net. If not, see <http://www.gnu.org/licenses/>.
  */
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Freeserf
 {
+    using System.Collections;
     using MapPos = System.UInt32;
+
+    class SearchNode
+    {
+        public SearchNode parent;
+        public uint gScore;
+        public uint fScore;
+        public MapPos pos;
+        public Direction dir;
+    }
+
+    class PriorityQueue<T> : IEnumerable<T> where T : class
+    {
+        IComparer<T> comparer;
+        T[] heap;
+
+        public int Count { get; private set; }
+
+        public PriorityQueue() 
+            : this(null)
+        {
+        }
+
+        public PriorityQueue(int capacity) 
+            : this(capacity, null)
+        {
+        }
+        public PriorityQueue(IComparer<T> comparer)
+            : this(128, comparer)
+        {
+        }
+
+        public PriorityQueue(int capacity, IComparer<T> comparer)
+        {
+            this.comparer = (comparer == null) ? Comparer<T>.Default : comparer;
+            heap = new T[capacity];
+        }
+
+        public void Push(T v)
+        {
+            if (Count >= heap.Length)
+                Array.Resize(ref heap, Count * 2);
+
+            heap[Count] = v;
+
+            SiftUp(Count++);
+        }
+
+        public T Pop()
+        {
+            var v = Top();
+            heap[0] = heap[--Count];
+
+            if (Count > 0)
+                SiftDown(0);
+
+            return v;
+        }
+
+        public T Top()
+        {
+            if (Count > 0)
+                return heap[0];
+
+            throw new InvalidOperationException("Heap is empty");
+        }
+
+        void SiftUp(int n)
+        {
+            var v = heap[n];
+
+            for (var n2 = n / 2; n > 0 && comparer.Compare(v, heap[n2]) > 0; n = n2, n2 /= 2)
+                heap[n] = heap[n2];
+
+            heap[n] = v;
+        }
+
+        void SiftDown(int n)
+        {
+            var v = heap[n];
+
+            for (var n2 = n * 2; n2 < Count; n = n2, n2 *= 2)
+            {
+                if (n2 + 1 < Count && comparer.Compare(heap[n2 + 1], heap[n2]) > 0)
+                    ++n2;
+
+                if (comparer.Compare(v, heap[n2]) >= 0)
+                    break;
+
+                heap[n] = heap[n2];
+            }
+
+            heap[n] = v;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)heap).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
 
     internal class Pathfinder
     {
+        // A search node is considered less than the other if
+        // it has a larger f-score. This means that in the max-heap
+        // the lower score will go to the top.
+        class SearchNodeComparer : IComparer<SearchNode>
+        {
+            public int Compare(SearchNode left, SearchNode right)
+            {
+                return right.fScore.CompareTo(left.fScore);
+            }
+        }
+
+        /* Find the shortest path from start to end (using A*) considering that
+           the walking time for a serf walking in any direction of the path
+           should be minimized. Returns a malloc'ed array of directions and
+           the size of this array in length. */
         public static Road Map(Map map, MapPos start, MapPos end, Road buildingRoad = null)
         {
-            // TODO
-            return null;
+            // Unfortunately the STL priority_queue cannot be used since we
+            // would need access to the underlying sequence to determine if
+            // a node is already in the open list. We keep instead open as
+            // a vector and apply std::pop_heap and std::push_heap to keep
+            // it heapified.
+            PriorityQueue<SearchNode> open = new PriorityQueue<SearchNode>(new SearchNodeComparer());
+            List<SearchNode> closed = new List<SearchNode>();
+
+            /* Create start node */
+            SearchNode node = new SearchNode()
+            {
+                pos = end,
+                gScore = 0,
+                fScore = HeuristicCost(map, start, end)
+            };
+
+            open.Push(node);
+
+            while (open.Count != 0)
+            {
+                node = open.Pop();
+
+                if (node.pos == start)
+                {
+                    /* Construct solution */
+                    Road solution = new Road();
+                    solution.Start(start);
+
+                    while (node.parent != null)
+                    {
+                        Direction dir = node.dir;
+                        solution.Extend(dir.Reverse());
+                        node = node.parent;
+                    }
+
+                    return solution;
+                }
+
+                /* Put current node on closed list. */
+                closed.Insert(0, node);
+
+                var cycle = DirectionCycleCW.CreateDefault();
+
+                foreach (Direction d in cycle)
+                {
+                    MapPos newPos = map.Move(node.pos, d);
+                    uint cost = ActualCost(map, node.pos, d);
+
+                    /* Check if neighbour is valid. */
+                    if (!map.IsRoadSegmentValid(node.pos, d) ||
+                        (map.GetObject(newPos) == global::Freeserf.Map.Object.Flag && newPos != start))
+                    {
+                        continue;
+                    }
+
+                    if ((buildingRoad != null) && buildingRoad.HasPos(map, newPos) &&
+                        (newPos != end) && (newPos != start))
+                    {
+                        continue;
+                    }
+
+                    /* Check if neighbour is in closed list. */
+                    if (closed.Any(n => n.pos == newPos))
+                        continue;
+
+                    /* See if neighbour is already in open list. */
+                    bool inOpen = false;
+                    
+                    foreach (var n in open)
+                    {
+                        if (n.pos == newPos)
+                        {
+                            inOpen = true;
+
+                            if (n.gScore >= node.gScore + cost)
+                            {
+                                n.gScore = node.gScore + cost;
+                                n.fScore = n.gScore + HeuristicCost(map, newPos, start);
+                                n.parent = node;
+                                n.dir = d;
+
+                                // Move element to the back and heapify
+                                open.Push(open.Pop());
+                            }
+
+                            break;
+                        }
+                    }
+
+                    /* If not found in the open set, create a new node. */
+                    if (!inOpen)
+                    {
+                        SearchNode newNode = new SearchNode();
+
+                        newNode.pos = newPos;
+                        newNode.gScore = node.gScore + cost;
+                        newNode.fScore = newNode.gScore + HeuristicCost(map, newPos, start);
+                        newNode.parent = node;
+                        newNode.dir = d;
+
+                        open.Push(newNode);
+                    }
+                }
+            }
+
+            return new Road();
+        }
+
+        static readonly uint[] walkCost = new uint[]
+        {
+            255u, 319u, 383u, 447u, 511u
+        };
+
+        static uint HeuristicCost(Map map, MapPos start, MapPos end)
+        {
+            /* Calculate distance to target. */
+            int distColumn = map.DistX(start, end);
+            int distRow = map.DistY(start, end);
+
+            int hDiff = Math.Abs((int)map.GetHeight(start) - (int)map.GetHeight(end));
+            int dist = 0;
+
+            if ((distColumn > 0 && distRow > 0) || (distColumn < 0 && distRow < 0))
+            {
+                dist = Math.Max(Math.Abs(distColumn), Math.Abs(distRow));
+            }
+            else
+            {
+                dist = Math.Abs(distColumn) + Math.Abs(distRow);
+            }
+
+            return (dist > 0) ? (uint)dist * walkCost[hDiff / dist] : 0u;
+        }
+
+        static uint ActualCost(Map map, MapPos pos, Direction dir)
+        {
+            MapPos otherPos = map.Move(pos, dir);
+
+            int hDiff = Math.Abs((int)map.GetHeight(pos) - (int)map.GetHeight(otherPos));
+
+            return walkCost[hDiff];
         }
     }
 }
