c002e311c4aff4ef279dbc3c884327b96daeeb87
Pyrdacor <trobt@web.de>
2018-12-20 17:17:19 +0100

    Corner stones and materials no longer appear in front of the building frame

--- Freeserf.Core/Render/RenderBuilding.cs
+++ Freeserf.Core/Render/RenderBuilding.cs
@@ -616,7 +616,7 @@ namespace Freeserf.Render
             {
                 crossOrStoneSprite.X = renderPosition.X + crossOrStoneSpriteOffset.X;
                 crossOrStoneSprite.Y = renderPosition.Y + crossOrStoneSpriteOffset.Y;
-                crossOrStoneSprite.BaseLineOffset = System.Math.Min(0, (sprite.Y + sprite.Height - 1) - (crossOrStoneSprite.Y + crossOrStoneSprite.Height));
+                crossOrStoneSprite.BaseLineOffset = System.Math.Min(0, (frameSprite.Y + frameSprite.Height + frameSprite.BaseLineOffset - 1) - (crossOrStoneSprite.Y + crossOrStoneSprite.Height));
             }
 
             var textureAtlas = TextureAtlasManager.Instance.GetOrCreate(Layer.Buildings);
@@ -630,7 +630,7 @@ namespace Freeserf.Render
             {
                 stones[i].X = renderPosition.X + materialSpriteInfos[0].Position.X + 10 - i * 3;
                 stones[i].Y = renderPosition.Y + materialSpriteInfos[0].Position.Y - 8 + i;
-                stones[i].BaseLineOffset = System.Math.Min(0, (frameSprite.Y + frameSprite.Height - 1) - (stones[i].Y + stones[i].Height));
+                stones[i].BaseLineOffset = System.Math.Min(0, (frameSprite.Y + frameSprite.Height + frameSprite.BaseLineOffset - 1) - (stones[i].Y + stones[i].Height));
             }
 
             // Planks waiting
@@ -638,7 +638,7 @@ namespace Freeserf.Render
             {
                 planks[i].X = renderPosition.X + materialSpriteInfos[1].Position.X + 12 - i * 3;
                 planks[i].Y = renderPosition.Y + materialSpriteInfos[1].Position.Y - 6 + i;
-                planks[i].BaseLineOffset = System.Math.Min(0, (frameSprite.Y + frameSprite.Height - 1) - (planks[i].Y + planks[i].Height));
+                planks[i].BaseLineOffset = System.Math.Min(0, (frameSprite.Y + frameSprite.Height + frameSprite.BaseLineOffset - 1) - (planks[i].Y + planks[i].Height));
             }
 
             if (building.IsBurning())
