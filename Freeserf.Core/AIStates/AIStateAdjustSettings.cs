ca79309855cd0c8763ad1d1aea4dcf9f497c4a57
Pyrdacor <trobt@web.de>
2019-02-28 12:39:56 +0100

    Adjust AI scythe toolmaker prio in hard times

--- Freeserf.Core/AIStates/AIStateAdjustSettings.cs
+++ Freeserf.Core/AIStates/AIStateAdjustSettings.cs
@@ -187,7 +187,7 @@ namespace Freeserf.AIStates
                 for (int i = 0; i < 9; ++i)
                     player.SetToolPriority(i, ushort.MinValue);
 
-                if (player.GetTotalBuildingCount(Building.Type.Fisher) != 0 && !(player.GetSerfCount(Serf.Type.Farmer) != 0 || game.HasAnyOfResource(player, Resource.Type.Scythe)))
+                if (player.GetSerfCount(Serf.Type.Farmer) == 0 && !game.HasAnyOfResource(player, Resource.Type.Scythe))
                 {
                     // set the priority for the scythe to 100%
                     player.SetToolPriority(Resource.Type.Scythe - Resource.Type.Shovel, ushort.MaxValue);
