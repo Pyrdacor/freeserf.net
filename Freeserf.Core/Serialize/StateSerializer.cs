833fa0a1dc039a220de71a312b601b445bbe2a39
Pyrdacor <trobt@web.de>
2020-01-29 13:28:30 +0100

    Fixed inverse Ignore attribute handling. Added missing header comments.

--- Freeserf.Core/Serialize/StateSerializer.cs
+++ Freeserf.Core/Serialize/StateSerializer.cs
@@ -1,3 +1,24 @@
+/*
+ * StateSerializer.cs - Serializer for state objects
+ *
+ * Copyright (C) 2019  Robert Schneckenhaus <robert.schneckenhaus@web.de>
+ *
+ * This file is part of freeserf.net. freeserf.net is based on freeserf.
+ *
+ * freeserf.net is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * freeserf.net is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with freeserf.net. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -50,7 +71,7 @@ namespace Freeserf.Serialize
             {
                 var map = new PropertyMap();
                 var properties = stateType.GetProperties()
-                    .Where(property => property.GetCustomAttribute(typeof(IgnoreAttribute)) != null);
+                    .Where(property => property.GetCustomAttribute(typeof(IgnoreAttribute)) == null);
 
                 foreach (var property in properties)
                 {
@@ -114,7 +135,7 @@ namespace Freeserf.Serialize
 
             var propertyMap = PropertyMap.Create(state.GetType());
             var properties = full ? state.GetType().GetProperties()
-                .Where(property => property.GetCustomAttribute(typeof(IgnoreAttribute)) != null)
+                .Where(property => property.GetCustomAttribute(typeof(IgnoreAttribute)) == null)
                 .Select(property => property.Name) : state.DirtyProperties;
 
             foreach (var property in properties)
