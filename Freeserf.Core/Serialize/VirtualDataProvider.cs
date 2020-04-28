6c06d1bebf93b9d6ad824a6cf0fdad7e10de88c3
Pyrdacor <trobt@web.de>
2020-04-28 09:56:58 +0200

    Added missing file

---
+++ Freeserf.Core/Serialize/VirtualDataProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Freeserf.Serialize
+{
+    /// <summary>
+    /// This is used for serializable classes which contains
+    /// members of a base class type that can change their
+    /// types at runtime to any derived class type.
+    /// </summary>
+    internal interface IVirtualDataProvider : IState
+    {
+        List<string> ChangeVirtualDataMembers { get; }
+    }
+}
